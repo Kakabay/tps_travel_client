75bce22e4b77e62bfca7cc1ceb3a4175455b009c
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-04-15 19:46:05 +0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      black: '#080808',
+      white: '#ffffff',
+      blue: '#213A73',
+      lightBlue: '#E9ECF1',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
